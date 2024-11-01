4b5cb38fc5c457cead5165bbcbd88e93f3eb85d9
crispy-caesus <114518720+crispy-caesus@users.noreply.github.com>
2024-11-01 13:11:12 +0100

    just a little testing

--- index.go
+++ index.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-        "log"
-        "net/http"
-        "html/template"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 type Page struct {
-        Title string
-        Body []byte
+	Title string
+	Body  []byte
 }
 
 var templates = template.Must(template.ParseFiles("index.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-        err := templates.ExecuteTemplate(w, tmpl+".html", p)
-        if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-        p := &Page{Title: "index"}
-        renderTemplate(w, "index", p)
+	p := &Page{Title: "index"}
+	renderTemplate(w, "index", p)
 }
-
 
 func main() {
-        http.HandleFunc("/", viewHandler)
-        log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
